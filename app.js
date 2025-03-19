15610b7bb0dcbd321299cc928fadf835b7a4ab3b
Nikos Kapetanidis <kapetanidis@synelixis.com>
2025-03-19 12:02:45 +0200

    Add global error handler

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const AppError = require('./utils/appError');
 
 const productRouter = require("./routes/productRoutes");
+const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
 
@@ -15,5 +16,7 @@ app.use("/api/v1/products", productRouter);
 app.all('*', (req, res, next) => {
     next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
+
+app.use(globalErrorHandler);
 
 module.exports = app;
