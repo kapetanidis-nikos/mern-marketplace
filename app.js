e2ef23bd12b7817b35a94d1fe8de5201a8357fd6
Nikos Kapetanidis <kapetanidis@synelixis.com>
2025-03-19 13:41:09 +0200

    Create signupn operation

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const AppError = require('./utils/appError');
 
 const productRouter = require("./routes/productRoutes");
+const userRouter = require('./routes/userRoutes');
 const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
@@ -12,6 +13,7 @@ app.use(express.json());
 
 // Routes
 app.use("/api/v1/products", productRouter);
+app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
     next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
---
+++ controllers/authController.js
@@ -0,0 +1,37 @@
+const jwt = require('jsonwebtoken');
+const User = require('./../models/userModel');
+const catchAsync = require('./../utils/catchAsync');
+
+const signToken = id => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN
+  });
+};
+
+const createSendToken = (user, statusCode, res) => {
+  const token = signToken(user._id);
+
+  user.password = undefined;
+
+  res.status(statusCode).json({
+    status: 'success',
+    token,
+    data: {
+      user
+    }
+  });
+};
+
+exports.signup = catchAsync(async (req, res, next) => {
+  // Ensure that role was not passed from client
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm
+  });
+
+  createSendToken(newUser, 201, res);
+});
+
+
