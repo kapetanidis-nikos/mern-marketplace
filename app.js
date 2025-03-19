5ec53c5ee8bcf3a5edbaf4c975abf76fc5f42df8
Nikos Kapetanidis <kapetanidis@synelixis.com>
2025-03-19 10:25:52 +0200

    Add global unhandled route handler

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 
+const AppError = require('./utils/appError');
+
 const productRouter = require("./routes/productRoutes");
 
 const app = express();
@@ -9,5 +11,9 @@ app.use(express.json());
 
 // Routes
 app.use("/api/v1/products", productRouter);
+
+app.all('*', (req, res, next) => {
+    next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
 
 module.exports = app;
