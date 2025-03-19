0f2afd1f0c91f3247d2150197386935c247426d1
Nikos Kapetanidis <kapetanidis@synelixis.com>
2025-03-19 10:24:10 +0200

    Add exception/rejection handlers

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
+process.on('uncaughtException', err => {
+  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({ path: "./config.env" });
 const app = require("./app");
 
@@ -15,6 +21,14 @@ mongoose
   .catch((err) => console.error("❌ MongoDB Connection Error:", err));
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`📶 App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', err => {
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
