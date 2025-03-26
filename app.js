d92c36f9c362a121d1d866aa4b59fc6cb34faa82
Nikos Kapetanidis <kapetanidis@synelixis.com>
2025-03-26 16:30:33 +0200

    Add middleware to protect from XSS, injection attacks

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const rateLimit = require('express-rate-limit')
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean')
 
 const AppError = require('./utils/appError');
 
@@ -25,6 +27,12 @@ app.use('/api', limiter);
 
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: `10kb` }));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
 
 // Routes
 app.use('/api/v1/products', productRouter);
