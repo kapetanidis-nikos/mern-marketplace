7149315e70f01233530fe0e6bf34f4c0a4f3501a
Nikos Kapetanidis <kapetanidis@synelixis.com>
2025-04-08 15:46:20 +0300

    Client: Create Login Form

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,9 +1,11 @@
-const LoginPage = () => {
-    return (
-        <div className="text-red-200" >
-          Login Page
-        </div>
-      );
-    };
-    
-export default LoginPage;
+import LoginForm from "@/components/auth/LoginForm"
+
+export default function LoginPage() {
+  return (
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <LoginForm />
+      </div>
+    </div>
+    )
+}
