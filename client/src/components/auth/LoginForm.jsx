daabd0cf05a60051812a63b2e51b3b048b6c9706
kapetanidis-n <nkap@qnr.com.gr>
2025-10-01 16:26:47 +0300

    Client: Create CardWrapper component

--- client/src/components/auth/LoginForm.jsx
+++ client/src/components/auth/LoginForm.jsx
@@ -1,82 +1,49 @@
-import { cn } from "@/lib"
-import { Button } from "@/components/ui/button"
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import { Link } from "react-router-dom" 
-import { motion } from "framer-motion";
-import IconInput from "../general/IconInput"
-import { Mail } from "lucide-react"
+import { cn } from '@/lib';
+import { Button } from '@/components/ui/button';
+import CardWrapper from '../general/CardWrapper';
+import { Label } from '@/components/ui/label';
+import { Link } from 'react-router-dom';
+import IconInput from '../general/IconInput';
+import { Mail, LockKeyhole } from 'lucide-react';
 
-
-export default function LoginForm({
-    className,
-    ...props
-  }) {
-
-
+export default function LoginForm({ className, ...props }) {
   return (
-    <div className={cn("flex flex-col gap-6", className)} {...props}>
-    <motion.div
-			initial={{ opacity: 0, y: 20 }}
-			animate={{ opacity: 1, y: 0 }}
-			transition={{ duration: 0.5 }}
-			className='max-w-md w-full bg-gray-800 bg-opacity-50 backdrop-filter backdrop-blur-xl rounded-2xl shadow-xl overflow-hidden'
-		>
-      <Card>
-        <CardHeader>
-          <CardTitle className="text-2xl">Login</CardTitle>
-          <CardDescription>
-            Enter your email below to login to your account
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <form>
-            <div className="flex flex-col gap-6">
-              <div className="grid gap-2">
-                <Label htmlFor="email">Email</Label>
-                <Input
-                  id="email"
-                  type="email"
-                  placeholder="m@example.com"
-                  required
-                />
-              </div>
-              <div className="grid gap-2">
-                <div className="flex items-center">
-                  <Label htmlFor="password">Password</Label>
-                  <a
-                    href="#"
-                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
-                  >
-                    Forgot your password?
-                  </a>
-                </div>
-                <Input id="password" type="password" required />
-                <IconInput icon={Mail}
-						type='email'
-						placeholder='Email Address'/>
-              </div>
-              <Button type="submit" className="w-full">
-                Login
-              </Button>
+    <div className={cn('flex flex-col gap-4', className)} {...props}>
+      <CardWrapper
+        title={'Login'}
+        description={'Login in to your Account in order to use our service.'}
+        shouldAnimate={true}
+      >
+        <form>
+          <div className="flex flex-col gap-2">
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <IconInput icon={Mail} id="email" type="email" placeholder="Email Address" />
             </div>
-            <div className="mt-4 text-center text-sm">
-              Don&apos;t have an account?{" "}
-                <Link to="/signup" className="underline underline-offset-4">
-                Sign up
-                </Link>
+            <div className="grid gap-2">
+              <div className="flex items-center">
+                <Label htmlFor="password">Password</Label>
+                <a
+                  href="#"
+                  className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                >
+                  Forgot your password?
+                </a>
+              </div>
+              <IconInput icon={LockKeyhole} id="password" type="password" placeholder="********" />
             </div>
-          </form>
-        </CardContent>
-      </Card>
-      </motion.div>
+            <Button type="submit" className="w-full">
+              Login
+            </Button>
+          </div>
+          <div className="mt-4 text-center text-sm">
+            Don&apos;t have an account?{' '}
+            <Link to="/signup" className="underline underline-offset-4">
+              Sign up
+            </Link>
+          </div>
+        </form>
+      </CardWrapper>
     </div>
-  )
+  );
 }
---
+++ client/src/components/general/CardWrapper.jsx
@@ -0,0 +1,35 @@
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import { motion } from 'framer-motion';
+
+export default function CardWrapper({ title, description, shouldAnimate, children }) {
+  const content = (
+    <Card className="bg-gray-800/90">
+      <CardHeader>
+        <CardTitle className="text-3xl font-bold mb-6 text-center bg-gradient-to-r from-green-400 to-emerald-500 text-transparent bg-clip-text">
+          {title}
+        </CardTitle>
+        <CardDescription>{description}</CardDescription>
+      </CardHeader>
+      <CardContent>{children}</CardContent>
+    </Card>
+  );
+
+  return (
+    <>
+      {shouldAnimate ? (
+        <motion.div
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5 }}
+          className="max-w-md w-full backdrop-filter backdrop-blur-xl rounded-2xl shadow-xl overflow-hidden"
+        >
+          {content}
+        </motion.div>
+      ) : (
+        <div className="max-w-md w-full backdrop-filter backdrop-blur-xl rounded-2xl shadow-xl overflow-hidden">
+          {content}
+        </div>
+      )}
+    </>
+  );
+}
